85215d3b557585e068e6175df5fb29c634793919
KevinM226 <kmmst226@mail.rmu.edu>
2020-10-03 18:19:36 -0400

    Update IotApplication.java
    i added comment

--- IoT-Backend/src/main/java/com/myproject/iot/IotApplication.java
+++ IoT-Backend/src/main/java/com/myproject/iot/IotApplication.java
@@ -1,5 +1,6 @@
 package com.myproject.iot;
 //new comment
+//comment part 2
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
