0e239033780a40972b3386ee5b5e3e35fca5c2e0
Dp237515 <71341406+Dp237515@users.noreply.github.com>
2020-11-08 23:06:02 -0500

    This fixed dubplicate conName

--- IoT-Backend/src/main/java/com/myproject/iot/controller/Controller.java
+++ IoT-Backend/src/main/java/com/myproject/iot/controller/Controller.java
@@ -23,7 +23,7 @@ public class Controller {
 
     @PostMapping("/")
     public ResponseEntity<Device> addDevice(@RequestBody CreateDevicePayload payload) {
-        return new ResponseEntity<>(deviceService.addDevice(payload.getName(), payload.getConName(),
+        return new ResponseEntity<>(deviceService.addDevice(payload.getName(), payload.getMacAdd(),
                 payload.getConName()), HttpStatus.CREATED);
     }
 
