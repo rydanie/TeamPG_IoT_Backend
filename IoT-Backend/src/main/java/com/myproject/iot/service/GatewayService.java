216bdebaa2bba11cf1794fb6de296d271668a18f
Dp237515 <71341406+Dp237515@users.noreply.github.com>
2020-11-14 19:19:52 -0500

    Edit Setup Gateway Backend

--- IoT-Backend/src/main/java/com/myproject/iot/service/GatewayService.java
+++ IoT-Backend/src/main/java/com/myproject/iot/service/GatewayService.java
@@ -1,12 +1,14 @@
 package com.myproject.iot.service;
 //comment
 //import all the things
+import com.myproject.iot.domain.Device;
 import com.myproject.iot.domain.Gateway;
 import com.myproject.iot.repository.GatewayRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class GatewayService {
@@ -24,6 +26,20 @@ public class GatewayService {
 
     public void deleteGateway(Long id){
         gatewayRepository.deleteById(id);
+    }
+
+    public Gateway getGateway(Long id){
+        Optional<Gateway> optionalGateway = gatewayRepository.findById(id);
+        if(optionalGateway.isPresent()){
+            return optionalGateway.get();
+        }
+        else{
+            return new Gateway();
+        }
+    }
+
+    public Gateway save(Gateway gateway){
+        return gatewayRepository.save(gateway);
     }
 
     public List<Gateway> getGateways() {
